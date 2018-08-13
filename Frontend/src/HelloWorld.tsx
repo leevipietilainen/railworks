0c87afee174c07788154f52c7928f57bbe83b433
unknown <ekowc.crowzenbruhi@solciel.com>
2018-08-13 17:24:36 +0900

    vscode buildi toimimaan

---
+++ Frontend/src/HelloWorld.tsx
@@ -0,0 +1,14 @@
+import React = require("react");
+
+class HelloWorld extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          test
+        </div>
+      );
+    }
+  }
+
+  export = HelloWorld;
---
+++ Frontend/src/Start.tsx
@@ -0,0 +1,9 @@
+import React = require("react");
+import ReactDOM = require("react-dom");
+
+import HelloWorld = require('./HelloWorld');
+
+ReactDOM.render(
+        <HelloWorld />,
+        document.getElementById('root')
+      );
