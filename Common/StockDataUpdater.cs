3fd0308743ad5c20b0b20d0ebe3b35321252983b
unknown <ekowc.crowzenbruhi@solciel.com>
2018-08-23 14:26:26 +0900

    Added boilerplate stock data updater class

---
+++ Common/StockDataUpdater.cs
@@ -0,0 +1,14 @@
+using System;
+using RailWorks.Common.Repository;
+using RailWorks.Common.Models;
+
+namespace RailWorks.Common
+{
+    class StockDataUpdater
+    {
+        public void UpdateStockSymbolData(StockSymbol Symbol)
+        {
+
+        }
+    }
+}
